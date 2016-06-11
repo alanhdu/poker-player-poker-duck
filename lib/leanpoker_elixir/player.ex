bb6cbae56a972b2686f135e24bde2c341770e81d
Alan Du <alanhdu@gmail.com>
2016-06-11 11:19:30 -0700

    Wait for it 2.0

--- lib/leanpoker_elixir/player.ex
+++ lib/leanpoker_elixir/player.ex
@@ -1,15 +1,16 @@
 require IEx;
 
 defmodule LeanpokerElixir.Player do
-  @version "0.0.1"
+  @version "1"
   def bet_request(game_state) do
     us = game_state["players"]
          |> List.to_tuple
          |> elem(game_state["in_action"])
     min_bet = game_state["current_buy_in"] - us["bet"]
 
-    if length(game_state["community_cards"]) < 4 do
-      min_bet
+
+    if game_state["round"] < 2 do
+      0
     else
       us["stack"]
     end
