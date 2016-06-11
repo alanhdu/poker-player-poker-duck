ee6970bef87ef0a1d1bc058bb2893df5e9443242
Alan Du <alanhdu@gmail.com>
2016-06-11 10:55:14 -0700

    All in, all day, every day

--- lib/leanpoker_elixir/player.ex
+++ lib/leanpoker_elixir/player.ex
@@ -1,7 +1,12 @@
+require IEx;
+
 defmodule LeanpokerElixir.Player do
   @version "0.0.1"
   def bet_request(game_state) do
-    0
+    us = game_state["players"]
+         |> List.to_tuple
+         |> elem(game_state["in_action"])
+    us["stack"]
   end
 
   def showdown(game_state) do
