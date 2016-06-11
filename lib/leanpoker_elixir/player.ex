f913ab14edfa32d8c7a5580cc100e07b0fba03c2
Alan Du <alanhdu@gmail.com>
2016-06-11 13:35:58 -0700

    Shuffle the deck

--- lib/leanpoker_elixir/player.ex
+++ lib/leanpoker_elixir/player.ex
@@ -1,7 +1,7 @@
 require IEx;
 
 defmodule LeanpokerElixir.Player do
-  @version "1.61"
+  @version "1.618"
   def bet_request(game_state) do
     [p1, p2, p3] = game_state["players"]
     us = elem({p1, p2, p3}, game_state["in_action"])
@@ -12,8 +12,8 @@ defmodule LeanpokerElixir.Player do
     half_chips = round(us["stack"] / 2)
     tenth_chips = round(us["stack"] / 100)
     cond do
+      c1["rank"] == c2["rank"] -> Enum.max(min_bet, half_chips + Enum.random 1..half_chips)
       min_bet < tenth_chips    -> min_bet
-      c1["rank"] == c2["rank"] -> half_chips + Enum.random 1..half_chips
       true                     -> 0
     end
   end
